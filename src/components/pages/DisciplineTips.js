a16cf60eb9fb85fe99aa2a815eeb1c229f81517b
Rediet0888 <redietsol17@gmail.com>
2022-05-05 09:23:23 +0300

    discipline

--- src/components/pages/DisciplineTips.js
+++ src/components/pages/DisciplineTips.js
@@ -3,7 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,7 +30,7 @@ export default function DisciplineTips() {
       </AppBar>
       <Grid>
           <Paper>
-              
+
           </Paper>
       </Grid>
     </div>
