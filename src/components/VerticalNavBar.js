1ad0391cc684973391cbb2e0af7b998ad725f630
Binalfew Kassa <binalfew.k.mekonnen@go.com.mt>
2022-06-25 18:10:28 +0300

    Refactor

--- src/components/VerticalNavBar.js
+++ src/components/VerticalNavBar.js
@@ -1,38 +1,31 @@
-import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import clsx from 'clsx';
 import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
+import React from 'react';
 //import Rules from "../files/Rules and Regulation for students.pdf"
 
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
-import ForumIcon from '@material-ui/icons/Forum';
-import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
-import Report from './Report';
-import Grid from '@material-ui/core/Grid';
-import { Paper } from '@material-ui/core';
-import ReportTable from './ReportTable';
-import { Link } from 'react-router-dom'
-import DescriptionIcon from '@material-ui/icons/Description';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import Toolbar from '@material-ui/core/Toolbar';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import DescriptionIcon from '@material-ui/icons/Description';
+import EmojiObjectsIcon from '@material-ui/icons/EmojiObjects';
 import LiveHelpIcon from '@material-ui/icons/LiveHelp';
+import NoteAddIcon from '@material-ui/icons/NoteAdd';
+import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
+import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 
@@ -96,16 +89,16 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
-   account: {
-     flex: 1,
-     flexDirection: 'row',
-     justifyContent: 'flex-end',
-     float: 'left'
+  account: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    float: 'left',
   },
 }));
 
 export default function VerticalNavBar(props) {
-    const classes = useStyles();
+  const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const [auth, setAuth] = React.useState(true);
@@ -117,126 +110,221 @@ export default function VerticalNavBar(props) {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-    // const news = [
-    //     { n: 'red' },
-    //     { n: 'rehd' },
-    //     { n: 'redj' },]
-    let role = window.localStorage.getItem("role_type");
+  // const news = [
+  //     { n: 'red' },
+  //     { n: 'rehd' },
+  //     { n: 'redj' },]
+  let role = window.localStorage.getItem('role_type');
 
-    let NavBarItems = [{ id: 1, name: 'Dashboard', icon: DashboardIcon , path:"/t"},
-    { id: 2, name: 'Profile', icon: AccountCircleIcon, path:"/Profile"},
-    { id: 3, name: 'Notification', icon: NotificationsOutlinedIcon, path:"/StudentDashboard" },
-    { id: 4, name: 'ScheduleHearing', icon: AccessTimeIcon , path:"/ScheduleHearing"},
-    { id: 5, name: 'AddScholarshipNews', icon: NoteAddIcon , path:"/AddScholarshipNews"},
-    { id: 6, name: 'Rules&Regulation', icon: DescriptionIcon , path:"/RulesAndRegulation"},
-    { id: 7,name: 'CreateAccusation', icon: NoteAddIcon , path:"/IncidentRegForm"},
-    { id: 8,name: 'AddPrefermentNews', icon: NoteAddIcon , path:"/AddPrefermentNews"},
-    { id: 9,name: 'Complain', icon: LiveHelpIcon, path:"/Complain" },
-     { id: 10,name: 'Students', icon: LiveHelpIcon, path:"/Student" },
-     { id: 11,name: 'Employees', icon: LiveHelpIcon, path:"/Employee" }
+  let NavBarItems = [
+    { id: 1, name: 'Dashboard', icon: DashboardIcon, path: '/t' },
+    { id: 2, name: 'Profile', icon: AccountCircleIcon, path: '/Profile' },
+    {
+      id: 3,
+      name: 'Notification',
+      icon: NotificationsOutlinedIcon,
+      path: '/StudentDashboard',
+    },
+    {
+      id: 4,
+      name: 'ScheduleHearing',
+      icon: AccessTimeIcon,
+      path: '/ScheduleHearing',
+    },
+    {
+      id: 5,
+      name: 'AddScholarshipNews',
+      icon: NoteAddIcon,
+      path: '/AddScholarshipNews',
+    },
+    {
+      id: 6,
+      name: 'Rules&Regulation',
+      icon: DescriptionIcon,
+      path: '/RulesAndRegulation',
+    },
+    {
+      id: 7,
+      name: 'CreateAccusation',
+      icon: NoteAddIcon,
+      path: '/IncidentRegForm',
+    },
+    {
+      id: 8,
+      name: 'AddPrefermentNews',
+      icon: NoteAddIcon,
+      path: '/AddPrefermentNews',
+    },
+    { id: 9, name: 'Complain', icon: LiveHelpIcon, path: '/Complain' },
+    { id: 10, name: 'Students', icon: LiveHelpIcon, path: '/Student' },
+    { id: 11, name: 'Employees', icon: LiveHelpIcon, path: '/Employee' },
+  ];
+  if (role === 'Student-Admin') {
+    NavBarItems = [
+      {
+        id: 1,
+        name: 'Dashboard',
+        icon: DashboardIcon,
+        path: '/StudentAdminDashboard',
+      },
+      { id: 2, name: 'Profile', icon: AccountCircleIcon, path: '/Profile' },
+      {
+        id: 3,
+        name: 'Notification',
+        icon: NotificationsOutlinedIcon,
+        path: '/NotificationOfStudentAdmin',
+      },
+      {
+        id: 4,
+        name: 'ScheduleHearing',
+        icon: AccessTimeIcon,
+        path: '/ScheduleHearing',
+      },
+      {
+        id: 5,
+        name: 'AddScholarshipNews',
+        icon: NoteAddIcon,
+        path: '/AddScholarshipNews',
+      },
+      {
+        id: 6,
+        name: 'Rules&regulation',
+        icon: DescriptionIcon,
+        path: '/RulesAndRegulation',
+      },
+      { id: 10, name: 'Students', icon: LiveHelpIcon, path: '/Student' },
+    ];
+  } else if (role === 'Employee-Admin') {
+    NavBarItems = [
+      {
+        id: 1,
+        name: 'Dashboard',
+        icon: DashboardIcon,
+        path: '/EmployeeAdminDashboard',
+      },
+      { id: 2, name: 'Profile', icon: AccountCircleIcon, path: '/Profile' },
+      {
+        id: 3,
+        name: 'Notification',
+        icon: NotificationsOutlinedIcon,
+        path: '/NotificationOfEmployeeAdmin',
+      },
+      {
+        id: 4,
+        name: 'ScheduleHearing',
+        icon: AccessTimeIcon,
+        path: '/ScheduleHearing',
+      },
+      {
+        id: 8,
+        name: 'AddPrefermentNews',
+        icon: NoteAddIcon,
+        path: '/AddPrefermentNews',
+      },
+      {
+        id: 6,
+        name: 'Rules&Regulation',
+        icon: DescriptionIcon,
+        path: '/RulesAndRegulation',
+      },
+      { id: 11, name: 'Employees', icon: LiveHelpIcon, path: '/Employee' },
+    ];
+  } else if (role === 'Student') {
+    NavBarItems = [
+      { id: 1, name: 'Dashboard', icon: DashboardIcon, path: '/t' },
+      { id: 2, name: 'Profile', icon: AccountCircleIcon, path: '/Profile' },
+      {
+        id: 3,
+        name: 'Notification',
+        icon: NotificationsOutlinedIcon,
+        path: '/NotificationOfStudents',
+      },
+      { id: 9, name: 'Complain', icon: LiveHelpIcon, path: '/Complain' },
+      {
+        id: 5,
+        name: 'SelfDisciplineTips',
+        icon: EmojiObjectsIcon,
+        path: '/DisciplineTips',
+      },
+      {
+        id: 6,
+        name: 'Rules&Regulation',
+        icon: DescriptionIcon,
+        path: '/RulesAndRegulation',
+      },
+    ];
+  } else if (role === 'Employee') {
+    NavBarItems = [
+      {
+        id: 1,
+        name: 'Dashboard',
+        icon: DashboardIcon,
+        path: '/EmployeeDashboard',
+      },
+      { id: 2, name: 'Profile', icon: AccountCircleIcon, path: '/Profile' },
+      {
+        id: 3,
+        name: 'Notification',
+        icon: NotificationsOutlinedIcon,
+        path: '/NotificationOfEmployees',
+      },
+      { id: 9, name: 'Complain', icon: LiveHelpIcon, path: '/Complain' },
+      {
+        id: 5,
+        name: 'SelfDisciplineTips',
+        icon: EmojiObjectsIcon,
+        path: '/DisciplineTips',
+      },
+      {
+        id: 6,
+        name: 'Rules&Regulation',
+        icon: DescriptionIcon,
+        path: '/RulesAndRegulation',
+      },
+      {
+        id: 7,
+        name: 'CreateAccusation',
+        icon: NoteAddIcon,
+        path: '/IncidentRegForm',
+      },
+    ];
+  }
 
-
-];
-    if (role === "Student-Admin") {
-       NavBarItems = [{ id: 1, name: 'Dashboard', icon: DashboardIcon , path:"/StudentAdminDashboard"},
-       { id: 2, name: 'Profile', icon: AccountCircleIcon, path:"/Profile" },
-       { id: 3, name: 'Notification', icon: NotificationsOutlinedIcon, path:"/NotificationOfStudentAdmin" },
-        { id: 4, name: 'ScheduleHearing', icon: AccessTimeIcon, path:"/ScheduleHearing" },
-        { id: 5, name: 'AddScholarshipNews', icon: NoteAddIcon , path:"/AddScholarshipNews" },
-         { id: 6, name: 'Rules&regulation', icon: DescriptionIcon , path:"/RulesAndRegulation"},
-        { id: 10,name: 'Students', icon: LiveHelpIcon, path:"/Student" }
-
-        ]
-     } else if (role === "Employee-Admin" ) {
-        NavBarItems = [{ id: 1,name: 'Dashboard', icon: DashboardIcon , path:"/EmployeeAdminDashboard"},
-       { id: 2,name: 'Profile', icon: AccountCircleIcon ,path:"/Profile" },
-       { id: 3,name: 'Notification', icon: NotificationsOutlinedIcon, path:"/NotificationOfEmployeeAdmin" },
-       { id: 4,name: 'ScheduleHearing', icon: AccessTimeIcon , path:"/ScheduleHearing"},
-       { id: 8,name: 'AddPrefermentNews', icon: NoteAddIcon , path:"/AddPrefermentNews"},
-       { id: 6,name: 'Rules&Regulation', icon: DescriptionIcon , path:"/RulesAndRegulation"},
-      { id: 11,name: 'Employees', icon: LiveHelpIcon, path:"/Employee" },
-
-      ]
-     } else if (role === "Student") {
-         NavBarItems = [{ id: 1,name: 'Dashboard', icon: DashboardIcon, path:"/t" },
-         { id: 2,name: 'Profile', icon: AccountCircleIcon, path:"/Profile" },
-         { id: 3,name: 'Notification', icon: NotificationsOutlinedIcon, path:"/NotificationOfStudents" },
-         { id: 9,name: 'Complain', icon: LiveHelpIcon, path:"/Complain" },
-         { id: 5,name: 'SelfDisciplineTips', icon: EmojiObjectsIcon,path:"/DisciplineTips"},
-         { id: 6,name: 'Rules&Regulation', icon: DescriptionIcon, path:"/RulesAndRegulation" },]
-     } else if (role === "Employee" ) {
-         NavBarItems = [{id: 1, name: 'Dashboard', icon: DashboardIcon, path:"/EmployeeDashboard" },
-        { id: 2,name: 'Profile', icon: AccountCircleIcon , path:"/Profile"},
-        { id: 3,name: 'Notification', icon: NotificationsOutlinedIcon , path:"/NotificationOfEmployees"},
-        { id: 9,name: 'Complain', icon: LiveHelpIcon , path:"/Complain"},
-        { id: 5,name: 'SelfDisciplineTips', icon: EmojiObjectsIcon , path:"/DisciplineTips"},
-        { id: 6,name: 'Rules&Regulation', icon: DescriptionIcon , path:"/RulesAndRegulation"},
-        { id: 7,name: 'CreateAccusation', icon: NoteAddIcon , path:"/IncidentRegForm"},]
-     }
-
-    console.log(NavBarItems)
-    return  (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar
+  console.log(NavBarItems);
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
         position="fixed"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
         })}
       >
         <Toolbar>
+          {auth && (
+            <div>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, {
+                  [classes.hide]: open,
+                })}
+              >
+                <MenuIcon />
+              </IconButton>
 
-      {auth && (
-        <div>
-        <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        onClick={handleDrawerOpen}
-        edge="start"
-        className={clsx(classes.menuButton, {
-          [classes.hide]: open,
-        })}
-      >
-        <MenuIcon />
-        </IconButton>
-
-
-        
-        <div className='classes.account'>
-        <AccountCircle />
-        <h>{role}</h>
-</div>
-</div>
-      )}
-      {/* //here goes the profile */}
+              <div className="classes.account">
+                <AccountCircle />
+                <h3>{role}</h3>
+              </div>
+            </div>
+          )}
+          {/* //here goes the profile */}
         </Toolbar>
       </AppBar>
-            {/* <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar sx={{ backgroundColor: 'red' }}>
-                    <Grid container xs={12} alignItems='flex-end'>
-                        <Grid xs={10}>
-                            <Typography variant="h6" noWrap>
-                                {role}
-                            </Typography>
-                        </Grid>
-                        <Grid xs={2}>
-                            <TextField id="standard-search" label="Search field" type="search" />
-                        </Grid>
-                    </Grid>
-
-                </Toolbar>
-            </AppBar> */}
-            {/* <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-                anchor="left"
-            >
-                <div className={classes.toolbar} />
-                <Divider /> */}
-                <Drawer
+      <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
@@ -251,51 +339,25 @@ export default function VerticalNavBar(props) {
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === 'rtl' ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
-                <List>
-                    {NavBarItems.map((menu) => (
-                        <div key={menu.id}>
-                             <Link className={classes.noDecoration} to={menu.path} >
-                            <ListItem button key={menu.name}>
-                                <ListItemIcon>{[<menu.icon />]}</ListItemIcon>
-                                <ListItemText primary={menu.name} />
-                            </ListItem>
-                        </Link>
-
-                        </div>
-                    ))}
-                </List>
-
-            </Drawer>
-            </div>
-            )}
-            {/*<main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Grid xs={12} container spacing={3}>
-
-                    {news.map((i) => {
-                        return (
-                            <Grid item xs={4}>
-                                <Report />
-                            </Grid>
-
-                        );
-                    }
-
-                    )}
-                    <Grid item xs={12} >
-                        <Paper className={classes.paper}>
-                            <ReportTable />
-                        </Paper>
-                    </Grid>
-
-
-                </Grid>
-                </main>*/}
-                {/* <p>Hi there</p>
-        </div>
-    ): <div></div>; */}
-
+        <List>
+          {NavBarItems.map((menu) => (
+            <Link className={classes.noDecoration} to={menu.path} key={menu.id}>
+              <ListItem button>
+                <ListItemIcon>{[<menu.icon />]}</ListItemIcon>
+                <ListItemText primary={menu.name} />
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+}
--- src/components/pages/NotificationOfStudents.js
+++ src/components/pages/NotificationOfStudents.js
@@ -1,25 +1,39 @@
-import { Typography, Grid,Paper } from '@material-ui/core'
-import React from 'react'
+import { Grid, Paper, Typography } from '@material-ui/core';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const NotificationOfStudents = () => {
+  const [notifications, setNotifications] = useState([]);
 
-  
-  const notifyStyle={padding :20,height:'30vh',width:200, margin:"20px auto"}
-  return (
-    <div>
-          <Paper elevation={10} style={notifyStyle}>
-      <Grid>
-        New Message
-      </Grid>
-     <Grid>
-      <Typography>Date and Time</Typography>
-     </Grid>
-     <Grid>
-      <Typography>Accusation Detail</Typography>
-     </Grid>
+  useEffect(() => {
+    const baseURL = 'http://localhost:3000/notification';
+    axios
+      .get(baseURL)
+      .then((response) => response.data)
+      .then((data) => {
+        setNotifications(data.post);
+      });
+  }, []);
+
+  const notifyStyle = {
+    padding: 20,
+    height: '30vh',
+    width: 200,
+    margin: '20px auto',
+  };
+  return notifications.map((notification) => (
+    <div style={{ marginTop: 100 }} key={notification.id}>
+      <Paper elevation={10} style={notifyStyle}>
+        <Grid>{notification.notification_title}</Grid>
+        <Grid>
+          <Typography>{notification.notification_type}</Typography>
+        </Grid>
+        <Grid>
+          <Typography>{notification.content}</Typography>
+        </Grid>
       </Paper>
     </div>
-  )
-}
+  ));
+};
 
 export default NotificationOfStudents;
